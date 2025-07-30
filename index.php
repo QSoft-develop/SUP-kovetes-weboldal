a83bbf4bc00c4bbdbe982ffe06a02728ccfb8a3b
3xcuse <68470233+3xcuse@users.noreply.github.com>
2025-07-30 23:33:19 +0200

    Apply new tile design to index.html

--- index.php
+++ index.php
@@ -7,108 +7,142 @@ function get_ver_info($code) {
     $path = __DIR__."/RegiForraskod/{$file}";
     return array(
         qsGetVersion($path, $code),
-        qsGetDate($path, $code)
+        qsGetDate($path, $code),
+        qsGetFileSize(__DIR__.'/RegiForraskod/FileS/'.module_files($code))
     );
+}
+
+function module_files($code) {
+    switch($code) {
+        case 'SUP': return 'SUP_Upd_Setup.exe';
+        case 'RAKTAR': return 'RA_Upd_Setup.exe';
+        case 'MERLEG': return 'LM_Upd_Setup.exe';
+        case 'TIP': return 'TIP_Upd_Setup.exe';
+        case 'XLS': return 'SUP_XLS_NET_Setup.exe';
+        case 'DBCONNECTOR': return 'DBConnector_Setup.exe';
+        case 'DBCONNECTORAPI': return 'DbConnectorApi.jar';
+        case 'QSBACKUPFDBSERVICE': return 'QsBackupFdbService.zip';
+        case 'RUSTDESK': return 'RustDeskQsoft.exe';
+        case 'FIREBIRD': return 'FB30_QSoft_Setup.exe';
+        case 'WEBUPDATE': return 'WebUpdate.exe';
+        default: return '';
+    }
 }
 ?>
 <!DOCTYPE html>
 <html lang="hu">
 <head>
 <meta charset="utf-8">
-<title>Linkek listája</title>
-<style>
-table { border-collapse: collapse; }
-th, td { border: 1px solid #444; padding: 8px; }
-</style>
+<title>SUP követés letöltések</title>
+<link rel="stylesheet" href="style.css">
+<script src="script.js" defer></script>
 </head>
 <body>
-<h1>Linkek gyűjteménye</h1>
-<table>
-<tr><th>Link</th><th>Adatok</th></tr>
-<tr>
-<td><a href="https://www.sup.hu/kov">https://www.sup.hu/kov</a></td>
-<td></td>
-</tr>
-<tr>
-<td><a href="http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd</a></td>
-<td></td>
-</tr>
-<tr>
-<td><a href="http://www.w3.org/1999/xhtml">http://www.w3.org/1999/xhtml</a></td>
-<td></td>
-</tr>
-<tr>
-<td><a href="http://ICON.png">http://ICON.png</a></td>
-<td></td>
-</tr>
-<tr>
-<td><a href="http://www.sup.hu/kov">http://www.sup.hu/kov</a></td>
-<td></td>
-</tr>
-<tr>
-<td><img src="RegiForraskod/kepek/Blue32x32.png" alt="Blue32x32"> <a href="https://aral.qsoft.hu/d/d11f48cbc61548619ad0/">https://aral.qsoft.hu/d/d11f48cbc61548619ad0/</a></td>
-<td></td>
-</tr>
-<tr>
-<td><a href="https://dl.sup.hu/dl/?file=supa016iso">https://dl.sup.hu/dl/?file=supa016iso</a></td>
-<td></td>
-</tr>
-<?php list($v,$d)=get_ver_info('SUP'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/sup.jpg" alt="SUP"> <a href="RegiForraskod/FileS/SUP_Upd_Setup.exe">SUP_Upd_Setup.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('RAKTAR'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/raktar.jpg" alt="RAKTAR"> <a href="RegiForraskod/FileS/RA_Upd_Setup.exe">RA_Upd_Setup.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('MERLEG'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/merleg.jpg" alt="MERLEG"> <a href="RegiForraskod/FileS/LM_Upd_Setup.exe">LM_Upd_Setup.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('TIP'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/tip.jpg" alt="TIP"> <a href="RegiForraskod/FileS/TIP_Upd_Setup.exe">TIP_Upd_Setup.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('XLS'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/xls.jpg" alt="XLS"> <a href="RegiForraskod/FileS/SUP_XLS_NET_Setup.exe">SUP_XLS_NET_Setup.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('DBCONNECTOR'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/dbconnector.jpg" alt="DBCONNECTOR"> <a href="RegiForraskod/FileS/DBConnector_Setup.exe">DBConnector_Setup.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('DBCONNECTORAPI'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/dbconnectorapi.jpg" alt="DBCONNECTORAPI"> <a href="RegiForraskod/FileS/DbConnectorApi.jar">DbConnectorApi.jar</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('QSBACKUPFDBSERVICE'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/qsfdb.png" alt="QSBACKUPFDBSERVICE"> <a href="RegiForraskod/FileS/QsBackupFdbService.zip">QsBackupFdbService.zip</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('RUSTDESK'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/tavman.jpg" alt="RUSTDESK"> <a href="RegiForraskod/FileS/RustDeskQsoft.exe">RustDeskQsoft.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('FIREBIRD'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/firebird.jpg" alt="FIREBIRD"> <a href="RegiForraskod/FileS/FB30_QSoft_Setup.exe">FB30_QSoft_Setup.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-<?php list($v,$d)=get_ver_info('WEBUPDATE'); ?>
-<tr>
-<td><img src="RegiForraskod/kepek/webupdate.jpg" alt="WEBUPDATE"> <a href="RegiForraskod/FileS/WebUpdate.exe">WebUpdate.exe</a></td>
-<td><ul><li>Verzió: <?php echo $v; ?></li><li>A feltöltés dátuma: <?php echo $d; ?></li></ul></td>
-</tr>
-</table>
+<header>
+    <img src="RegiForraskod/kepek/suplogo.jpg" alt="SUP logo" class="logo">
+    <h1>SUP követés letöltések</h1>
+</header>
+<main>
+    <section class="tiles-row large">
+        <?php list($v,$d,$s)=get_ver_info('SUP'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('SUP'); ?>" class="tile" data-name="SUP" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('SUP'); ?>">
+            <img src="RegiForraskod/kepek/sup.jpg" alt="SUP">
+            <span class="name">SUP</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+        <?php list($v,$d,$s)=get_ver_info('RAKTAR'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('RAKTAR'); ?>" class="tile" data-name="Raktár" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('RAKTAR'); ?>">
+            <img src="RegiForraskod/kepek/raktar.jpg" alt="Raktár">
+            <span class="name">Raktár</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+        <?php list($v,$d,$s)=get_ver_info('MERLEG'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('MERLEG'); ?>" class="tile" data-name="Mérleg" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('MERLEG'); ?>">
+            <img src="RegiForraskod/kepek/merleg.jpg" alt="Mérleg">
+            <span class="name">Mérleg</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+        <?php list($v,$d,$s)=get_ver_info('TIP'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('TIP'); ?>" class="tile" data-name="TIP" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('TIP'); ?>">
+            <img src="RegiForraskod/kepek/tip.jpg" alt="TIP">
+            <span class="name">TIP</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+    </section>
+    <section class="tiles-row">
+        <?php list($v,$d,$s)=get_ver_info('XLS'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('XLS'); ?>" class="tile" data-name="SUP Xls.NET" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('XLS'); ?>">
+            <img src="RegiForraskod/kepek/xls.jpg" alt="XLS">
+            <span class="name">SUP Xls.NET</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+        <?php list($v,$d,$s)=get_ver_info('DBCONNECTOR'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('DBCONNECTOR'); ?>" class="tile" data-name="DbConnector" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('DBCONNECTOR'); ?>">
+            <img src="RegiForraskod/kepek/dbconnector.jpg" alt="DbConnector">
+            <span class="name">DbConnector</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+        <?php list($v,$d,$s)=get_ver_info('DBCONNECTORAPI'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('DBCONNECTORAPI'); ?>" class="tile" data-name="DbConnector API" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('DBCONNECTORAPI'); ?>">
+            <img src="RegiForraskod/kepek/dbconnectorapi.jpg" alt="DbConnector API">
+            <span class="name">DbConnector API</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+        <?php list($v,$d,$s)=get_ver_info('QSBACKUPFDBSERVICE'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('QSBACKUPFDBSERVICE'); ?>" class="tile" data-name="QsFdbBackupService" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('QSBACKUPFDBSERVICE'); ?>">
+            <img src="RegiForraskod/kepek/qsfdb.png" alt="QsFdbBackupService">
+            <span class="name">QsFdbBackupService</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+    </section>
+    <section class="tiles-row">
+        <?php list($v,$d,$s)=get_ver_info('RUSTDESK'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('RUSTDESK'); ?>" class="tile" data-name="RustDesk" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('RUSTDESK'); ?>">
+            <img src="RegiForraskod/kepek/tavman.jpg" alt="RustDesk">
+            <span class="name">RustDesk</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+        <?php list($v,$d,$s)=get_ver_info('FIREBIRD'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('FIREBIRD'); ?>" class="tile" data-name="Firebird SQL" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('FIREBIRD'); ?>">
+            <img src="RegiForraskod/kepek/firebird.jpg" alt="Firebird">
+            <span class="name">Firebird SQL</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+        <?php list($v,$d,$s)=get_ver_info('WEBUPDATE'); ?>
+        <a href="RegiForraskod/FileS/<?php echo module_files('WEBUPDATE'); ?>" class="tile" data-name="WebUpdate" data-version="<?php echo $v; ?>" data-date="<?php echo $d; ?>" data-size="<?php echo $s; ?>" data-file="<?php echo module_files('WEBUPDATE'); ?>">
+            <img src="RegiForraskod/kepek/webupdate.jpg" alt="WebUpdate">
+            <span class="name">WebUpdate</span>
+            <span class="version"><?php echo $v; ?></span>
+            <span class="date"><?php echo $d; ?></span>
+        </a>
+    </section>
+</main>
+<div id="modal" class="modal">
+    <div class="modal-content">
+        <span id="modal-close" class="close">&times;</span>
+        <h2 class="modal-title"></h2>
+        <p>File: <span class="modal-file"></span></p>
+        <p>Verzió: <span class="modal-version"></span></p>
+        <p>Dátum: <span class="modal-date"></span></p>
+        <p>Méret: <span class="modal-size"></span></p>
+        <div class="modal-buttons">
+            <a class="modal-download" href="#">Letöltés</a>
+        </div>
+    </div>
+</div>
+<footer>
+    <p>&copy; QSoft Kft. 1991-<?php echo date('Y'); ?>. Minden jog fenntartva.</p>
+    <p><a href="https://www.sup.hu">Fő weboldal</a> | <a href="https://www.facebook.com/sup.hu">Facebook</a></p>
+</footer>
 </body>
 </html>
-
