cecc937ad2711806fa0910e06e39cacc2c83e7cd
3xcuse <68470233+3xcuse@users.noreply.github.com>
2025-07-31 15:38:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $all_modules = array_merge($rows['large1'], $rows['large2'], $rows['small']);
 <body>
 <header>
     <h1>SUP követés letöltések</h1>
-    <img src="kepek/suplogo.jpg" alt="SUP logo" class="logo">
+    <img src="kepek/suplogo.png" alt="SUP logo" class="logo">
 </header>
 <main>
     <div class="grid">
